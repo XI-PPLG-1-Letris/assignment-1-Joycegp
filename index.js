473a92c9204089ee185297501b28970a974e3846
Joycegp <ghanimajoyce@gmail.com>
2025-10-06 09:55:07 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+//penjumlahan 
+const tambah = (a,b) => {
+    return a + b;
+}
+console.log(tambah(2,8));
+
+//pengurangan
+const kurang = (a,b) => {
+    return a - b;
+}
+console.log(kurang(10,5));
+
+//perkalian
+const kali = (a,b) => {
+    return a*b;
+}
+console.log(kali(3,4));
+
+//pembagian
+const bagi = (a,b) => {
+    return a / b;
+}
+console.log(bagi(12,2));
+
+//perpangkatan
+const pangkat = (a,b) => {
+    return a ** b;
+}
+console.log(pangkat(5,5))
+
+//modulus
+const mod = (a, b) => {
+    return a % b;
+}
+console.log(mod(10, 3));
